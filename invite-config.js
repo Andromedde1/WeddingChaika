32a20507ec7ae87608dc70dc6da29789d27d2cb1
Andromedde1 <rybakovanton733@gmail.com>
2026-05-30 23:49:07 +0400

    Update invite-config.js

--- invite-config.js
+++ invite-config.js
@@ -6,7 +6,7 @@
 window.INVITE_CONFIG = {
   /** Строки на обложке */
   coverLine1: 'Дорогая Виктория',
-  coverLine2: 'Мы приглашаем тебя',
+  coverLine2: 'Мы приглашаем тебя ',
   coverLine3: 'на нашу свадьбу',
 
   /** Первая строка (coverLine1) уходит в Formspree как «кому_приглашение» */
